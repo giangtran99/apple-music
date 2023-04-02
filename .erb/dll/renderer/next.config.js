a8fab3e3218e9b49780008db0f05c96e9deea6a5
giang.tranz <hoanggiangg199@gmail.com>
2023-04-02 11:29:08 +0700

    Delete .erb/dll directory

--- .erb/dll/renderer/next.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-};
-module.exports = nextConfig;
-//# sourceMappingURL=next.config.js.map
--- .erb/dll/renderer/src/pages/_document.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const document_1 = require("next/document");
-function Document() {
-    return ((0, jsx_runtime_1.jsxs)(document_1.Html, { lang: "en", children: [(0, jsx_runtime_1.jsx)(document_1.Head, {}), (0, jsx_runtime_1.jsxs)("body", { children: [(0, jsx_runtime_1.jsx)(document_1.Main, {}), (0, jsx_runtime_1.jsx)(document_1.NextScript, {})] })] }));
-}
-exports.default = Document;
-//# sourceMappingURL=_document.js.map
